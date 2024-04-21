21ad25088b8019707f8e6461838cdbcaa705905f
mohsen w elsisi <mohsenelsisi36@gmail.com>
2024-04-21 21:17:39 +0200

    vite pwa installed

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "flash card app",
+        display: "fullscreen",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
